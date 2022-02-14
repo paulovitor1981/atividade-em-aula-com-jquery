7032d0a86f9a725107ae3ae6278ec855d968f35c
paulovitor1981 <paulo.cdvitor@hotmail.com>
2022-02-13 22:12:24 -0300

    atividadeem aula com js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+
+
+let email = document.getElementById("campo-email");
+
+function enviarEmail(){
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
+
+let listaNoticias = [
+    {
+        titulo:"jogos novos",
+        descricao:"Todas as segundas feiras emos novos jogos para dowload ",
+    },
+    {
+        titulo:"jogos que voce gosta",
+        descricao:"esses são os jogos mais preferidos para dowload ",
+    },
+    {
+        titulo:"jogos para crianças",
+        descricao:"jogos que as crianças vao gostar jogos para dowload ",
+    },
+]
+
+function renderizarNoticias(){
+    let espaco = document.getElementById("espaco-noticias")
+
+    let template = "";
+    
+    for (let index = 0; index < listaNoticias.length; index++) {
+        const noticias = listaNoticias [index];
+
+        template += `<div id="espaco-noticias" class="comoparticipar">
+        <h1 >${noticias.titulo}</h1></div>
+       <div class="noticias">
+        <p>${noticias.descricao}</p>
+        </div>
+            
+    </div>`
+    }
+       espaco.innerHTML = template; 
+    }
+    
